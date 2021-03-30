d69a93187c6b88f5ba0bd7ae881908a412f34365
Tomas Meneses <tomasmeneses22@gmail.com>
2021-03-30 01:43:29 -0300

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState }  from 'react';
+import React, { useCallback }  from 'react';
 import GlobalStyle from '../src/styles/GlobalStyle';
 import Home from './pages/Home'
 
